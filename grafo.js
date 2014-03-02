882b806c4cb783c85cd0b6d2dd32a2e2da90b372
Javier Moreno <bluelephant@gmail.com>
2014-03-02 16:05:16 -0500

    Correcciones

--- grafo.js
+++ grafo.js
@@ -55,27 +55,6 @@ d3.csv("candidatos.csv", function(error, data) {
   y.domain(d3.extent(data, function(d) { return d.y_coord; })).nice();
   radius.domain(d3.extent(data, function(d){return d.seguidorespol; })).nice();
   color.domain(d3.extent(data, function(d){return d.partido;}));
-/*  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Sepal Width (cm)");
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Sepal Length (cm)")*/
 
   svg.selectAll(".dot")
       .data(data)
